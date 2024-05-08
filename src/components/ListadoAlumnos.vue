898983f238a3731a184d102c4af9504fc707901b
Mateos Joaquin <jmateos@andreani.com>
2024-05-08 14:17:54 -0300

    computed active

--- src/components/ListadoAlumnos.vue
+++ src/components/ListadoAlumnos.vue
@@ -4,18 +4,25 @@ import { ref, computed } from 'vue'
 let id = 1
 
 const newAlumno = ref('')
+const mostrarInactivos = ref(false)
 const listadoAlumnos = ref([
-    {id: id++, name: 'Alumno 1'},
-    {id: id++, name: 'Alumno 2'},
-    {id: id++, name: 'Alumno 3'},
-    {id: id++, name: 'Alumno 4'}
+    {id: id++, name: 'Alumno 1', active: true},
+    {id: id++, name: 'Alumno 2', active: true},
+    {id: id++, name: 'Alumno 3', active: true},
+    {id: id++, name: 'Alumno 4', active: true}
 ])
+
+const listadoAlumnosFiltrados = computed(()=>{
+    return mostrarInactivos.value ?
+     listadoAlumnos.value :
+      listadoAlumnos.value.filter((a)=> a.active)
+})
 
 function agregarAlumno() {
     if(newAlumno.value == ''){
         return
     }
-    listadoAlumnos.value.push({id: id++, name: newAlumno.value})
+    listadoAlumnos.value.push({id: id++, name: newAlumno.value, active: true})
     newAlumno.value = ''
 }
 
@@ -29,11 +36,19 @@ function quitarAlumno(alumno) {
     <input type="text" v-model="newAlumno" placeholder="Nuevo Alumno" />
     <button @click="agregarAlumno">Agregar Alumno</button>
     <ul>
-        <li v-for="alumno in listadoAlumnos" :key="alumno.id">
-        {{ alumno.id }}: {{ alumno.name }} <button @click="quitarAlumno(alumno)">X</button>
+        <li v-for="alumno in listadoAlumnosFiltrados" :key="alumno.id">
+            <input type="checkbox" v-model="alumno.active" >
+            <span :class="{inactive: !alumno.active}">{{ alumno.id }}: {{ alumno.name }}</span>
+            <button @click="quitarAlumno(alumno)">X</button>
         </li>
     </ul>
+    <input type="checkbox" v-model="mostrarInactivos">Ver Inactivos
+    <br/>
+    <button @click="mostrarInactivos = !mostrarInactivos">Mostrar Inactivos</button>
 </template>
 
 <style scoped>
+.inactive{
+    text-decoration: line-through
+}
 </style>
